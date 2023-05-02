e6bb988a9f5d04a719321555643bc18c62cc7fca
Wyatt Ptak <107960001+Kino8724@users.noreply.github.com>
2023-05-02 19:14:07 +0000

    Added variables; talking about let and const

---
+++ variables.js
@@ -0,0 +1,8 @@
+// Initiates a variable; weakly typed
+let Age = 72;
+
+// Initiates a constant variable; cannot be changed
+const Name = "Wyatt";
+
+// Makes a variable; not used anymore; can be insecure
+var height = 5;
