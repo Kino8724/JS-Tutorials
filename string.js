51b3e9a76e81e18c9fa2f1760cbb035d40a193e9
Wyatt Ptak <107960001+Kino8724@users.noreply.github.com>
2023-05-02 19:37:11 +0000

    Final changes to String

--- string.js
+++ string.js
@@ -14,4 +14,15 @@ animals.indexOf("z") // Returns -1 (not found)
 
 let baseball = "baseball";
 baseball.slice(0,4) // "base" (start, up to [not including])
+baseball.slice(4) // "ball" (start [goes to end if one arg])
 
+let sport = "football is the best";
+sport.replace("football", "Jiu Jitsu"); // "Jiu Jitsu is the best"
+
+// String escape characters use a backslash (\) before the character
+    // New line (\n)
+    // Tab (\t)
+    // Backslash (\\)
+
+// Use backticks for Template Literals
+let templateLiteral = `${msg}` // This will take the msg variable into the string
