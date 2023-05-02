e82305ed4d762a040d4e5d3b0b2edfe0b2c6a9f8
Wyatt Ptak <107960001+Kino8724@users.noreply.github.com>
2023-05-02 19:33:40 +0000

    Working with Strings in JS

---
+++ string.js
@@ -0,0 +1,17 @@
+// This is going to just be a place to practice and look at String methods in JS
+
+let msg = "Hello World";
+msg.toUpperCase(); // HELLO WORLD
+msg.toLowerCase(); // hello world
+
+let color = "    red      ";
+color.trim() // Removes leading and trailing whitespaces
+
+let animals = "catdog";
+animals.indexOf("cat") // Returns 0
+animals.indexOf("dog") // Returns 3
+animals.indexOf("z") // Returns -1 (not found)
+
+let baseball = "baseball";
+baseball.slice(0,4) // "base" (start, up to [not including])
+
